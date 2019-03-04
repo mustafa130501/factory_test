001f3064b88e6b2dc681e92003814cb3324eb5de
mustafa130501 <48223438+mustafa130501@users.noreply.github.com>
2019-03-05 01:45:00 +0300

    umut oldu mu

--- src/main/java/com/project/factory/Test.java
+++ src/main/java/com/project/factory/Test.java
@@ -3,9 +3,9 @@ package com.project.factory;
 public class Test {
 
 	public static void main(String[] args) {
-    System.out.println("HELLOOO");
-    ///LOW UMUT
-    System.out.println("low ata");
+    System.out.println("hello umut");
+    
+   
 	}
 
 }
